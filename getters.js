4e2a6e7e80738815fa360a78f2eb6a2466d0f4bf
angelaebstewart <angelaebstewart@users.noreply.github.com>
2017-04-29 19:09:48 -0400

    Get Counties Median income updates the bar chart

--- getters.js
+++ getters.js
@@ -1,6 +1,24 @@
+/*
+var countyData;
+var nationalData;
+
+d3.json("countyData.json", function(error, data) {
+		if(error) {
+				console.log(error);
+		}
+		countyData = data;
+});
+
+d3.json("nationalData.json", function(error, data) {
+		if(error) {
+				console.log(error);
+		}
+		nationalData = data;
+});*/
+
 function getSexData(data, id) {
 	var sexData = [
-			{'label' : 'Female', 'value' : data[id].female}, 
+			{'label' : 'Female', 'value' : data[id].female},
             {'label' : 'Male', 'value' : data[id].male}];
 
 	return sexData;
@@ -8,24 +26,42 @@ function getSexData(data, id) {
 
 function getRaceData(data, id) {
 	var raceData = [
-			{'label' : 'Asian', 'value' : data[id].asian}, 
+			{'label' : 'Asian', 'value' : data[id].asian},
             {'label' : 'Black', 'value' : data[id].black},
             {'label' : 'Hawaiian', 'value' : data[id].hawaiian},
             {'label' : 'Mixed', 'value' : data[id].mixed},
             {'label' : 'Native American', 'value' : data[id].native},
             {'label' : 'Other', 'value' : data[id].other},
             {'label' : 'White', 'value' : data[id].white}];
-
 	return raceData;
 }
 
-function getCountiesMedianIncome(data, id) {
-	console.log("in getCountiesMedianIncome");
+function getCountiesMedianIncome(data,id) {
+	var incomeData = [];
+
+	for (var s in data)
+	{
+	    if (data[s].abbreviation == id)
+			{
+				var county = data[s];
+				var countyData = {'label' : county["county"], 'value' : county["income"]};
+				incomeData.push(countyData);
+			}
+	}
+
+	return incomeData;
 }
 
-function getStatesMedianIncome(data, id) {
+function getStatesMedianIncome(data,id) {
 	console.log("in getStatesMedianIncome");
 }
 
-
-
+function getCountiesCrimeRate(data, id, crime) {
+	/*
+		Nick, add function here. 
+		Data is the incoming county data JSON file
+		ID is the state ID
+		Crime is the type of crime (propery, violent, overall, burglary, homicide, etc...)
+		return an array just like getCountiesMeidanIncome where each index's label is a county name and value is crime rate of the crime
+	*/
+}

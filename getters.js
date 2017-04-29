6acb115e154dcfe1c218db4e1e60cccfc2eea97d
angelaebstewart <angelaebstewart@users.noreply.github.com>
2017-04-29 10:06:49 -0400

    getRaceData() added

--- getters.js
+++ getters.js
@@ -4,6 +4,17 @@ function getSexData(data, id) {
             {'label' : 'Male', 'value' : data[id].male}];
 
 	return sexData;
-
 }
 
+function getRaceData(data, id) {
+	var raceData = [
+			{'label' : 'Asian', 'value' : data[id].asian}, 
+            {'label' : 'Black', 'value' : data[id].black},
+            {'label' : 'Hawaiian', 'value' : data[id].hawaiian},
+            {'label' : 'Mixed', 'value' : data[id].mixed},
+            {'label' : 'Native American', 'value' : data[id].native},
+            {'label' : 'Other', 'value' : data[id].other},
+            {'label' : 'White', 'value' : data[id].white}];
+
+	return raceData;
+}
